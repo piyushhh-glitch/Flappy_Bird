9d5302caf1e8269db3c8dcb7a4579436f48c23ca
piyushhh-glitch <piyushthakur1611@gmail.com>
2026-07-27 10:49:27 +0530

    Implemented epsilon greedy and decay

--- agent.py
+++ agent.py
@@ -1,11 +1,14 @@
+from Flappy_Bird import dqn
 import flappy_bird_gymnasium
 import gymnasium as gym
 import os 
 import torch
-import torch.nn
+import torch.nn as nn
 from dqn import DQN
 from experience_replay import ReplayMemory
-
+import itertools
+import yaml
+import torch.optim as optim
 
 if torch.backends.mps.is_available:
     device="mps"
@@ -15,35 +18,76 @@ else:
     device="cpu"
 
 
+class Agent:
 
-def run(self,is_training=True, render=False):
-    env = gym.make("FlappyBird-v0", render_mode="human" if render else None)
+    def __init__(self,param_set):
+        self.param_set=param_set
+        with open("parameters.yaml","r") as f:
+            all_parameter_set=yaml.safe_load(f)
+            params=all_parameter_set[param_set]
 
-    num_states=env.observation_space.shape[0] #input dim
+        self.alpha=params["alpha"]
+        self.gamma=params["gamma"]
 
-    num_actions=env.action_space.n #output dim
+        self.epsilon_init=params["epsilon_init"]
+        self.epsilon_min=params["epsilon_min"]
+        self.epsilon_decay=params["epsilon_decay"]
+
+        self.replay_memory_size=params["replay_memory_size"]
+        self.mini_batch_size=params["mini_batch_size"]
+
+        self.reward_threshold=params["reward_threshold"]
+        self.network_sync_rate=params["network_sync_rate"]
+
+        self.loss_fn=nn.MSELoss()
+        self.optimizer=None
 
 
-    policy_dqn=DQN(num_states,num_actions).to(device)
+    
+    def run(self,is_training=True, render=False):
 
-    state, _ = env.reset()
+        env = gym.make("FlappyBird-v0", render_mode="human" if render else None)
 
-    if is_training:
-        memory=ReplayMemory(10000)
+        num_states=env.observation_space.shape[0] #input dim
 
-    while True:
-        # Next action:
-        # (feed the observation to your agent here)
-        action = env.action_space.sample()
+        num_actions=env.action_space.n #output dim
+
+
+        policy_dqn=DQN(num_states,num_actions).to(device)
 
         if is_training:
-                memory.append((state,action,new_state,reward,terminated))
+            memory=ReplayMemory(self.replay_memory_size)
+            epsilon=self.epsilon_init
 
-        # Processing:terminated -> done 
-        next_state,reward, terminated, _, info = env.step(action)
-        
-        # Checking if the player is still alive
-        if terminated:
-            break
+        for episode in itertools.count():
 
-    env.close()
+            state, _ = env.reset()
+            state=torch.tensor(state,dtype=torch.float,device=device)
+
+            episode_rewards=0
+            terminated=False
+
+            while not terminated:
+                if is_training and random.random()<epsilon:
+                    action = env.action_space.sample() #explore
+                else:
+                    with torch.no_grad():
+                        action=policy_dqn(state,unsqueeze(dim=0)).squeeze().argmax() #exploit
+
+                # Processing:terminated -> done 
+                next_state,reward, terminated, _, _ = env.step(action.item())
+
+                reward=torch.tensor(reward,dtype=torch.float,device=device)
+                next_state=torch.tensor(next_state,dtype=torch.float,device=device)
+
+                if is_training:
+                    memory.append((state,action,new_state,reward,terminated))
+
+                
+                state=next_state
+                episode_rewards+=reward
+            print(f"For episode -> {episode+1}, total reward is -> {episode_rewards} and epsilon =>{epsilon}")
+
+            epsilon=max(epsilon*self.epsilon_decay,self.epsilon_min)
+
+        # env.close() - manually stop
